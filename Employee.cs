e2d8b662eb26a2ab7402c6934aab7f65c0b055d9
SlavaRedor4n <134692685+SlavaRedor4n@users.noreply.github.com>
2023-05-30 19:13:55 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika2023Day3
+{
+    internal class Employee
+    { 
+            private static int counter = 0;
+
+            private int id;
+            private string fullName;
+            private int department;
+            private decimal wage;
+
+            public Employee(string fullName, int department, decimal wage)
+            {
+                id = ++counter;
+                this.fullName = fullName;
+                this.department = department;
+                this.wage = wage;
+            }
+
+            public int Id
+            {
+                get { return id; }
+            }
+
+            public string FullName
+            {
+                get { return fullName; }
+            }
+
+            public int Department
+            {
+                get { return department; }
+                set { department = value; }
+            }
+
+            public decimal Wage
+            {
+                get { return wage; }
+                set { wage = value; }
+            }
+
+            public static void DisplayAllEmployees(Employee[] employees)
+            {
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine("ID: {0}, ФИО: {1}, Отдел: {2}, Зарплата: {3}", employee.Id, employee.FullName, employee.Department, employee.Wage);
+                }
+            }
+
+            public static decimal CalculateTotalWage(Employee[] employees)
+            {
+                decimal totalWage = 0;
+                foreach (Employee employee in employees)
+                {
+                    totalWage += employee.Wage;
+                }
+                return totalWage;
+            }
+
+            public static Employee FindEmployeeWithMinWage(Employee[] employees)
+            {
+                Employee employeeWithMinWage = employees[0];
+                for (int i = 1; i < employees.Length; i++)
+                {
+                    if (employees[i].Wage < employeeWithMinWage.Wage)
+                    {
+                        employeeWithMinWage = employees[i];
+                    }
+                }
+                return employeeWithMinWage;
+            }
+
+            public static Employee FindEmployeeWithMaxWage(Employee[] employees)
+            {
+                Employee employeeWithMaxWage = employees[0];
+                for (int i = 1; i < employees.Length; i++)
+                {
+                    if (employees[i].Wage > employeeWithMaxWage.Wage)
+                    {
+                        employeeWithMaxWage = employees[i];
+                    }
+                }
+                return employeeWithMaxWage;
+            }
+
+            public static decimal CalculateAverageWage(Employee[] employees)
+            {
+                decimal totalWage = CalculateTotalWage(employees);
+                decimal averageWage = totalWage / employees.Length;
+                return averageWage;
+            }
+
+            public static void DisplayAllEmployeeNames(Employee[] employees)
+            {
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine("ФИО: {0}", employee.FullName);
+                }
+                }
+            }
+        }
+    
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Praktika2023Day3;
+using System;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Employee[] employees = new Employee[10];
+
+        employees[0] = new Employee("Горбачева Наталья Николаевна", 5, 57000.00m);
+        employees[1] = new Employee("Приходько Степан Александрович", 3, 85000.00m);
+        employees[2] = new Employee("Семенов Дмитрий Анатольевич", 2, 30000.00m);
+        employees[3] = new Employee("Годунова Раиса Ивановна", 4, 63000.00m);
+        employees[4] = new Employee("Раскольников Сергей Семенович", 5, 20000.00m);
+        employees[5] = new Employee("Кацурова Мария Евгеньевна", 1, 27000.00m);
+        employees[6] = new Employee("Донской Роман Николаевич", 1, 90000.00m);
+        employees[7] = new Employee("Пирогов Степан Борисович", 4, 52000.00m);
+        employees[8] = new Employee("Дементьев Александр Артемович", 3, 78000.00m);
+        employees[9] = new Employee("Мизина Маргарита Дмитриевна", 5, 19000.00m);
+
+        Employee.DisplayAllEmployees(employees);
+
+        decimal totalWage = Employee.CalculateTotalWage(employees);
+        Console.WriteLine("Общая сумма затрат на зарплаты: {0}", totalWage);
+
+        Employee employeeWithMinWage = Employee.FindEmployeeWithMinWage(employees);
+        Console.WriteLine("Сотрудник с минимальной зарплатой: {0}", employeeWithMinWage.FullName);
+
+        Employee employeeWithMaxWage = Employee.FindEmployeeWithMaxWage(employees);
+        Console.WriteLine("Сотрудник с максимальной зарплатой: {0}", employeeWithMaxWage.FullName);
+
+        decimal averageWage = Employee.CalculateAverageWage(employees);
+        Console.WriteLine("Средняя зарплата: {0}", averageWage);
+
+        Employee.DisplayAllEmployeeNames(employees);
+        Console.ReadKey();
+    }
+}
