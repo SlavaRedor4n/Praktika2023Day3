65cdaa31c0d061cded4dc6ee1378742303123ab0
SlavaRedor4n <134692685+SlavaRedor4n@users.noreply.github.com>
2023-06-01 19:17:30 +0300

    Add files via upload

---
+++ Praktika2023Day3-main/Praktika2023Day3-main/Employee.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Linq;
+
+namespace Praktika2023Day3
+{
+    internal class Employee
+    {
+        private static int counter = 0;
+
+        private int id;
+        private string fullName;
+        private int department;
+        private decimal wage;
+
+        public Employee(string fullName, int department, decimal wage)
+        {
+            id = ++counter;
+            this.fullName = fullName;
+            this.department = department;
+            this.wage = wage;
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+        }
+
+        public int Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public decimal Wage
+        {
+            get { return wage; }
+            set { wage = value; }
+        }
+
+        public static void DisplayAllEmployees(Employee[] employees)
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine("ID: {0}, ФИО: {1}, Отдел: {2}, Зарплата: {3}", employee.Id, employee.FullName, employee.Department, employee.Wage);
+            }
+        }
+
+        public static decimal CalculateTotalWage(Employee[] employees)
+        {
+            decimal totalWage = employees.Sum(employee => employee.Wage);
+            return totalWage;
+        }
+
+        public static Employee FindEmployeeWithMinWage(Employee[] employees)
+        {
+            Employee employeeWithMinWage = employees[0];
+            for (int i = 1; i < employees.Length; i++)
+            {
+                if (employees[i].Wage < employeeWithMinWage.Wage)
+                {
+                    employeeWithMinWage = employees[i];
+                }
+            }
+            return employeeWithMinWage;
+        }
+
+        public static Employee FindEmployeeWithMaxWage(Employee[] employees)
+        {
+            Employee employeeWithMaxWage = employees[0];
+            for (int i = 1; i < employees.Length; i++)
+            {
+                if (employees[i].Wage > employeeWithMaxWage.Wage)
+                {
+                    employeeWithMaxWage = employees[i];
+                }
+            }
+            return employeeWithMaxWage;
+        }
+
+        public static decimal CalculateAverageWage(Employee[] employees)
+        {
+            decimal totalWage = CalculateTotalWage(employees);
+            decimal averageWage = totalWage / employees.Length;
+            return averageWage;
+        }
+
+        public static void DisplayAllEmployeeNames(Employee[] employees)
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine("ФИО: {0}", employee.FullName);
+            }
+        }
+
+        public static void IndexSalary(Employee[] employees, decimal percent)
+        {
+            foreach (Employee employee in employees)
+            {
+                decimal increaseAmount = employee.Wage * percent / 100;
+                employee.Wage += increaseAmount;
+            }
+        }
+
+        public static Employee FindEmployeeWithMinWageByDepartment(Employee[] employees, int department)
+        {
+            Employee[] employeesInDepartment = employees.Where(employee => employee.Department == department).ToArray();
+            return FindEmployeeWithMinWage(employeesInDepartment);
+        }
+
+        public static Employee FindEmployeeWithMaxWageByDepartment(Employee[] employees, int department)
+        {
+            Employee[] employeesInDepartment = employees.Where(employee => employee.Department == department).ToArray();
+            return FindEmployeeWithMaxWage(employeesInDepartment);
+        }
+
+        public static decimal CalculateAverageWageByDepartment(Employee[] employees, int department)
+        {
+            Employee[] employeesInDepartment = employees.Where(employee => employee.Department == department).ToArray();
+            return CalculateAverageWage(employeesInDepartment);
+        }
+
+        public static void IndexSalaryByDepartment(Employee[] employees, int department, decimal percent)
+        {
+            Employee[] employeesInDepartment = employees.Where(employee => employee.Department == department).ToArray();
+            IndexSalary(employeesInDepartment, percent);
+        }
+
+        public static void DisplayEmployeesInDepartment(Employee[] employees, int department)
+        {
+            Employee[] employeesInDepartment = employees.Where(employee => employee.Department == department).ToArray();
+            DisplayAllEmployees(employeesInDepartment);
+        }
+
+        public static void DisplayEmployeesWithLowerWage(Employee[] employees, decimal amount)
+        {
+            Employee[] employeesWithLowerWage = employees.Where(employee => employee.Wage < amount).ToArray();
+            foreach (Employee employee in employeesWithLowerWage)
+            {
+                Console.WriteLine("ID: {0}, ФИО: {1}, Зарплата: {2}", employee.Id, employee.FullName, employee.Wage);
+            }
+        }
+
+        public static void DisplayEmployeesWithHigherOrEqualWage(Employee[] employees, decimal amount)
+        {
+            Employee[] employeesWithHigherOrEqualWage = employees.Where(employee => employee.Wage >= amount).ToArray();
+            foreach (Employee employee in employeesWithHigherOrEqualWage)
+            {
+                Console.WriteLine("ID: {0}, ФИО: {1}, Зарплата: {2}", employee.Id, employee.FullName, employee.Wage);
+            }
+        }
+    }
+}
+
---
+++ Praktika2023Day3-main/Praktika2023Day3-main/Program.cs
@@ -0,0 +1,63 @@
+﻿using Praktika2023Day3;
+using System;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Employee[] employees = new Employee[10];
+
+        employees[0] = new Employee("Горбачева Наталья Николаевна", 5, 57000.00m);
+        employees[1] = new Employee("Приходько Степан Александрович", 3, 85000.00m);
+        employees[2] = new Employee("Семенов Дмитрий Анатольевич", 2, 30000.00m);
+        employees[3] = new Employee("Годунова Раиса Ивановна", 4, 63000.00m);
+        employees[4] = new Employee("Раскольников Сергей Семенович", 5, 20000.00m);
+        employees[5] = new Employee("Кацурова Мария Евгеньевна", 1, 27000.00m);
+        employees[6] = new Employee("Донской Роман Николаевич", 1, 90000.00m);
+        employees[7] = new Employee("Пирогов Степан Борисович", 4, 52000.00m);
+        employees[8] = new Employee("Дементьев Александр Артемович", 3, 78000.00m);
+        employees[9] = new Employee("Мизина Маргарита Дмитриевна", 5, 19000.00m);
+
+        Employee.DisplayAllEmployees(employees);
+
+        decimal totalWage = Employee.CalculateTotalWage(employees);
+        Console.WriteLine("Общая сумма затрат на зарплаты: {0}", totalWage);
+
+        Employee employeeWithMinWage = Employee.FindEmployeeWithMinWage(employees);
+        Console.WriteLine("Сотрудник с минимальной зарплатой: {0}", employeeWithMinWage.FullName);
+
+        Employee employeeWithMaxWage = Employee.FindEmployeeWithMaxWage(employees);
+        Console.WriteLine("Сотрудник с максимальной зарплатой: {0}", employeeWithMaxWage.FullName);
+
+        decimal averageWage = Employee.CalculateAverageWage(employees);
+        Console.WriteLine("Средняя зарплата: {0}", averageWage);
+
+        Employee.DisplayAllEmployeeNames(employees);
+
+        // Новые методы
+        decimal percent = 10;
+        Employee.IndexSalary(employees, percent);
+        Console.WriteLine("Зарплаты всех сотрудников увеличены на {0}%", percent);
+
+        int department = 5;
+        Employee employeeWithMinWageByDepartment = Employee.FindEmployeeWithMinWageByDepartment(employees, department);
+        Console.WriteLine("Сотрудник с минимальной зарплатой в отделе {0}: {1}", department, employeeWithMinWageByDepartment.FullName);
+
+        int department2 = 3;
+        Employee employeeWithMaxWageByDepartment = Employee.FindEmployeeWithMaxWageByDepartment(employees, department2);
+        Console.WriteLine("Сотрудник с максимальной зарплатой в отделе {0}: {1}", department2, employeeWithMaxWageByDepartment.FullName);
+
+        decimal averageWageByDepartment = Employee.CalculateAverageWageByDepartment(employees, department2);
+        Console.WriteLine("Средняя зарплата в отделе {0}: {1}", department2, averageWageByDepartment);
+
+        decimal amount = 50000;
+        Console.WriteLine("Сотрудники с зарплатой ниже {0}:", amount);
+        Employee.DisplayEmployeesWithLowerWage(employees, amount);
+
+        decimal amount2 = 60000;
+        Console.WriteLine("Сотрудники с зарплатой выше или равной {0}:", amount2);
+        Employee.DisplayEmployeesWithHigherOrEqualWage(employees, amount2);
+
+        Console.ReadKey();
+    }
+}
