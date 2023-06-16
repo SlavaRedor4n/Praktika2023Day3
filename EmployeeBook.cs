0be6e3a29db002008852fab21ad7d84fb8c2534a
SlavaRedor4n <134692685+SlavaRedor4n@users.noreply.github.com>
2023-06-16 15:31:11 +0300

    Add files via upload

--- EmployeeBook.cs
+++ EmployeeBook.cs
@@ -1,27 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Day3Update2Day5
 {
-    using System;
-    using System.Collections.Generic;
-
-    public class Employee
-    {
-        public string FullName { get; set; }
-        public int ID { get; set; }
-        public decimal Salary { get; set; }
-        public string Department { get; set; }
-    }
-
-    public class EmployeeBook
+    public class EmployeeBook1
     {
         private List<Employee> employees;
 
-        public EmployeeBook()
+        public EmployeeBook1()
         {
             employees = new List<Employee>();
         }
-
         public void AddEmployee(string fullName, int id, decimal salary, string department)
         {
             Employee employee = new Employee
@@ -94,28 +86,5 @@ namespace Day3Update2Day5
             return employees.Find(e => e.FullName == fullNameOrId || e.ID.ToString() == fullNameOrId);
         }
     }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            EmployeeBook employeeBook = new EmployeeBook();
-
-            employeeBook.AddEmployee("+Горбачева Наталья Николаевна", 1, 5000, "HR");
-            employeeBook.AddEmployee("+Приходько Степан Александрович", 2, 6000, "Финансовый отдел");
-            employeeBook.AddEmployee("+Семенов Дмитрий Анатольевич", 3, 5500, "Отдел IT");
-            employeeBook.AddEmployee("+Годунова Раиса Ивановна", 4, 5200, "Кадровый ");
-
-            employeeBook.PrintEmployeesByDepartments();
-
-            employeeBook.UpdateSalary("Горбачева Наталья Николаевна", 5500);
-            employeeBook.UpdateDepartment("Годунова Раиса Ивановна", "IT");
-
-            employeeBook.PrintEmployeesByDepartments();
-
-            employeeBook.RemoveEmployee("-Семенов Дмитрий Анатольевич");
-
-            employeeBook.PrintEmployeesByDepartments();
-        }
-    }
 }
+
